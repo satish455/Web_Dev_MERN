0889b54af838de29e28d75d8225d9c130f9c1ffc
satish tiwari <satishtiwari217@gmail.com>
2023-09-22 12:33:45 -0400

    second commit

--- JavaScript/Function/script.js
+++ JavaScript/Function/script.js
@@ -1,11 +1,72 @@
-function sum(){
-    var a=90;
-    var b=90;
+// function sum(){
+//     var a=90;
+//     var b=90;
 
-    ans=a+b;
+//     ans=a+b;
 
 
-    return ans;
-}
+//     return ans;
+// }
 
-console.log(sum());
+// console.log(sum());
+
+
+
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+
+// console.log(sum());
+
+
+// undefined + undefined ==not a number;
+
+
+
+// var a;
+
+// console.log(a);
+
+
+// it is undefined  
+
+// we can set a variable to undefined also by ourself 
+// thats why We set a variable to NULLLlllllllll
+
+
+
+
+
+// function sum(num1,num2){
+//     return num1+num2;
+// }
+// console.log(sum(10));
+
+// first 10 is assign to num1
+
+
+
+
+
+// function sum(num1,num2=10){
+//     return num1+num2;
+// }
+
+// console.log(sum(20));
+
+
+
+
+
+// function sum(num1=10,num2=30){
+//     return num1+num2;
+// }
+
+// console.log(sum(20,30));
+
+// if we pass parameter to function then default value is override
+
+
+
+
+
