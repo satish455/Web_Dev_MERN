f419348b06458ab367cd4a83123fadc160bc7ab6
satish tiwari <satishtiwari217@gmail.com>
2023-10-16 13:24:56 -0400

    commit on 16 oct

--- JavaScript/objects/script.js
+++ JavaScript/objects/script.js
@@ -12,11 +12,20 @@
 
 // unordered data structures
 
+// objects are used to group up similar thing in javascript it is data structure
 
 
 
+// properties key value pair
+
+// let todo={
+//      name: "satish",
+//      age:24
 
 
+// }
+
+// console.log(todo);
 
 
 
@@ -60,25 +69,71 @@
 
 
 
-let person={
-    naam:"satish kumar tivari",
+// let person={
+//     naam:"satish kumar tivari",
 
-    city:"delhi",
+//     city:"delhi",
 
-    describe:function(){
-        console.log("my name is satish kumar tivari")
-        console.log(`my name is ${person.naam}`)
-        console.log(`my name is ${this.naam}`)
-    }
-}
+//     describe:function(){
+//         console.log("my name is satish kumar tivari")
+//         console.log(`my name is ${person.naam}`)
+//         console.log(`my name is ${this.naam}`)
+//     }
+// }
 
 
-console.log(person.describe());
+// console.log(person.describe());
 
-let ans=person.describe();
+// let ans=person.describe();
 
-console.log(ans);
+// console.log(ans);
 
 // this keyword point to the object when it refered in the method
 
 // when nothing is returned from the function then bydefault it returns uncdefined
+
+
+
+// let todo={
+
+//     // properties key value pair
+
+//     name:"satish",
+//     age:24
+// }
+
+
+
+
+
+let todo={
+    // properties key value pair  unordered data structure
+    title:"Chapter book kro",
+    done:true,
+    ande:"nahi khana hai",
+    veg:"ho gye hai",
+
+    5:"yes",
+    s5:true
+    // 5s  wrong way
+
+}
+
+
+console.log(todo);
+
+console.log(todo['ande']);
+
+// todo["ande"];
+
+console.log(todo[5])
+console.log(todo['5'])
+
+console.log(todo['s5'])
+
+
+
+// numbers are super flexible in objects can directly accesed
+// never use numbers as key bts 
+
+// bts keys are stored in string and can accessed easily like array using a string array
